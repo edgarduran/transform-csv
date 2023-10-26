91be9ae5f54686a4244c8fce91a15a582e91a6b7
edgarduran <edgar.eduran@gmail.com>
2023-10-26 13:36:58 -0600

    add initial test

--- spec/transform_csv_spec.rb
+++ spec/transform_csv_spec.rb
@@ -1,5 +1,9 @@
-require 'transform_csv'
+require "transform_csv"
 
 describe TransformCsv do
-
+  describe "intialize" do
+    it "parses csv to array" do
+      expect(TransformCsv.new("input.csv")).not_to eq(nil)
+    end
+  end
 end
