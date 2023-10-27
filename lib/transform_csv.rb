2f0f477d487585663a2c8dcd34020d147b203224
edgarduran <edgar.eduran@gmail.com>
2023-10-27 09:43:23 -0600

    add data standardizing methods

--- lib/transform_csv.rb
+++ lib/transform_csv.rb
@@ -2,12 +2,17 @@ require "csv"
 require "pry"
 
 class TransformCsv
-  attr_accessor :file, :arr_of_rows
+  attr_accessor :arr_of_rows
+
+  STANDARDIZED_DATA_CSV = "output.csv"
 
   def initialize(filename)
+    # handle file not found
+    # !File.exists?(filename) rescue return "Error: file does not exist"
+
+
     @arr_of_rows = []
-    @file = CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
-      #  arr_of_rows << row.to_hash
+    CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
       first_name = row[:first_name]
       last_name = row[:last_name]
       dob = row[:dob]
@@ -16,28 +21,25 @@ class TransformCsv
       expiry_date = row[:expiry_date]
       phone_number = row[:phone_number]
 
-      first_name = first_name.nil? || first_name.empty? ? nil : standardize_string(first_name)
-      last_name = last_name.nil? || last_name.empty? ? nil : standardize_string(last_name)
-      dob = dob.nil? || dob.empty? ? nil : standardize_date(dob)
-      member_id = member_id.nil? || member_id.empty? ? nil : member_id
-      effective_date = effective_date.nil? || effective_date.empty? ? nil : standardize_date(effective_date)
-      expiry_date = expiry_date.nil? || expiry_date.empty? ? nil : standardize_date(expiry_date)
-      phone_number = phone_number.nil? || phone_number.empty? ? nil : standardize_phone_number(phone_number)
+      first_name = first_name.nil? || first_name.empty? ? "N/A": standardize_string(first_name)
+      last_name = last_name.nil? || last_name.empty? ? "N/A": standardize_string(last_name)
+      dob = dob.nil? || dob.empty? ? "N/A": standardize_date(dob)
+      member_id = member_id.nil? || member_id.empty? ? "N/A": member_id
+      effective_date = effective_date.nil? || effective_date.empty? ? "N/A": standardize_date(effective_date)
+      expiry_date = expiry_date.nil? || expiry_date.empty? ? "N/A": standardize_date(expiry_date)
+      phone_number = phone_number.nil? || phone_number.empty? ? "N/A": standardize_phone_number(phone_number)
+
+      arr_of_rows <<[first_name, last_name, dob, member_id, effective_date, expiry_date, phone_number]
     end
+    # create_standardized_csv
   end
 
   def standardize_string(str)
-    return str.class == String ? str.strip : nil
+    return str
   end
 
   def standardize_date(dob)
-    parsed = Date.parse(dob) rescue nil
-
-    if parsed
-      return parsed.strftime('%Y-%m-%d')
-    else
-      return nil
-    end
+    return dob
   end
 
   def standardize_phone_number(num)
